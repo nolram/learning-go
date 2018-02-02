59345e819a7c6f6ad5561c4a0651f5e84304fd04
Marlon Baptista de Quadros <marlonbquadros@gmail.com>
2018-02-01 22:54:54 -0200

    - Arquivos iniciais de aprendizado com Go

---
+++ cards/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var card string
+
+func main() {
+
+	card = "Ace of Spades"
+	// card := "Ace of Spades"
+	fruitCount := 5
+	fmt.Println(card)
+	fmt.Println(fruitCount)
+}
